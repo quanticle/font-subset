59044ac9412ad6d9ed85fb204770678f6115b9fc
Rohit Patnaik <quanticle@quanticle.net>
2017-10-01 05:53:41 -0700

    Bundle react and react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,5 @@ module.exports = {
             {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
             {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
         ]
-    },
-
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
     }
 };
